0be7bcbe3ede02307d2b394207cce9d3d1276d86
Rahul70321 <botturahul2@gmail.com>
2025-01-08 20:10:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recycling.db'
 db = SQLAlchemy(app)
 
